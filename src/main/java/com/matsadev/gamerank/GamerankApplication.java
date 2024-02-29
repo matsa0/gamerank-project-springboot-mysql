ea532ba86bc40cd885db9d1ec3b94c4bc0bdc769
matsa0 <az3vedomatheus@gmail.com>
2024-02-29 11:13:34 -0300

    First Commit

---
+++ src/main/java/com/matsadev/gamerank/GamerankApplication.java
@@ -0,0 +1,13 @@
+package com.matsadev.gamerank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matsadev/gamerank/GamerankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matsadev.gamerank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
